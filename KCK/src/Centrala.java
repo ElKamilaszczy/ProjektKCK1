778fff0e40ff5382503a7b4bdd90756893d48161
Kamil_Jarmoc <kam498@wp.pl>
2018-10-16 11:03:49 +0200

    Lekarz i Centrala

--- KCK/src/Centrala.java
+++ KCK/src/Centrala.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Centrala {
-
+	private List<Lekarz> lekarze = new ArrayList<Lekarz>();
+	private List<Pacjent> pacjenci = new ArrayList<Pacjent>();
+	private List<Recepta> recepty = new ArrayList<Recepta>();
+	private List<Skierowanie> skierowania = new ArrayList<Skierowanie>();
+	private List<Wizyta> wizyty = new ArrayList<Wizyta>();
+	public List<Lekarz> getLekarze() {
+		return lekarze;
+	}
+	public List<Pacjent> getPacjenci() {
+		return pacjenci;
+	}
+	public List<Recepta> getRecepty() {
+		return recepty;
+	}
+	public List<Skierowanie> getSkierowania() {
+		return skierowania;
+	}
+	public List<Wizyta> getWizyty() {
+		return wizyty;
+	}
+	
 }
--- KCK/src/Lekarz.java
+++ KCK/src/Lekarz.java
@@ -1,4 +1,24 @@
 
 public class Lekarz {
-
+	private static int total_id = 0;
+	private int id;
+	private String login;
+	private String haslo;
+	private String imie;
+	private String nazwisko;
+	private int wiek;
+	private int sala;
+	private String telefon;
+	
+	public Lekarz(String login, String haslo, String imie, Strong nazwisko, int wiek, int sala, String telefon)
+	{
+		this.id = total_id++;
+		this.login = login;
+		this.haslo = haslo;
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.wiek = wiek;
+		this.sala = sala;
+		this.telefon = telefon;
+	}
 }
