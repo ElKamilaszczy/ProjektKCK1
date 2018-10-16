6e02cb0b1070b834bfd80d69cdf359c9e7155568
kucharz96 <kucharz96@wp.pl>
2018-10-16 12:40:45 +0200

    metody list

--- KCK/src/Centrala.java
+++ KCK/src/Centrala.java
@@ -2,13 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 //siemka
 public class Centrala {
+	
 	private List<Lekarz> lekarze = new ArrayList<Lekarz>();
 	private List<Pacjent> pacjenci = new ArrayList<Pacjent>();
 	private List<Recepta> recepty = new ArrayList<Recepta>();
 	private List<Skierowanie> skierowania = new ArrayList<Skierowanie>();
 	private List<Wizyta> wizyty = new ArrayList<Wizyta>();
+	
+	
 	public List<Lekarz> getLekarze() {
 		return lekarze;
+	}
+	
+	public List<Pacjent> getPacjenci() {
+		return pacjenci;
+	}
+	public List<Recepta> getRecepty() {
+		return recepty;
+	}
+	public List<Skierowanie> getSkierowania() {
+		return skierowania;
+	}
+	public List<Wizyta> getWizyty() {
+		return wizyty;
 	}
 	public String Logowanie(String login, String haslo)
 	{
@@ -21,17 +37,36 @@ public class Centrala {
 		}
 		return "";
 	}
-	public List<Pacjent> getPacjenci() {
-		return pacjenci;
+	public void addLekarz(Lekarz e) {
+		lekarze.add(e);
 	}
-	public List<Recepta> getRecepty() {
-		return recepty;
+	public void removeLekarz(int index) {
+		lekarze.remove(index);
 	}
-	public List<Skierowanie> getSkierowania() {
-		return skierowania;
+	public void addPacjent(Pacjent e) {
+		pacjenci.add(e);
 	}
-	public List<Wizyta> getWizyty() {
-		return wizyty;
+	public void removePacjent(int index) {
+		pacjenci.remove(index);
+	}
+
+	public void addRecepta(Recepta e) {
+		recepty.add(e);
+	}
+	public void removeRecepta(int index) {
+		recepty.remove(index);
+	}
+	public void addSkierowanie(Skierowanie e) {
+		skierowania.add(e);
+	}
+	public void removeSkierowanie(int index) {
+		skierowania.remove(index);
+	}
+	public void addWizyta(Wizyta e) {
+		wizyty.add(e);
+	}
+	public void removeWizyta(int index) {
+		wizyty.remove(index);
 	}
 	
 }
